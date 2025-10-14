dd1b72cb0709ce23926659dacf71f8fc8503f2f1
DebuggerO1 <shivasharmagkp4479@gmail.com>
2025-10-14 15:36:59 +0530

    dom

---
+++ JAVA SCRIPT/ES6/dom.js
@@ -0,0 +1,6 @@
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+h1.innerHTML = "Python is a programming language";
+div.appendChild(h1);
+const root = document.getElementById('container'); 
+root.appendChild(div);
